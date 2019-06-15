c70987f155c6205f65198aa5103717e3c82499d1
BAANG <chcelestino@gmail.com>
2019-06-15 13:02:11 -0700

    Add basic animation through to blurbs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,9 +4,19 @@ var hideSplash = function() {
     console.log("Splash screen hidden...")
 }
 
+var removeAnimation = function() {
+    $(".center-label").removeClass("scale-in")
+}
+
 var showMain = function() {
     $("#main").show()
+    $(".center-label").addClass("scale-in")
+    setTimeout(removeAnimation, 1000)
     console.log("Main screen visible...")
+}
+
+var showBlurb = function(divId) {
+    $(divId).show()
 }
 
 //On document load...
@@ -16,5 +26,24 @@ $(document).ready(function(){
     //3s timeout to hide splash screen; reveal main page
     setTimeout(hideSplash, 3000);
     setTimeout(showMain, 3000);
+
+    $(document).on("click", ".center-label", function(event){
+        var clickID = event.target.id
+
+        if (clickID == "about-main") {
+            $("#portfolio-main, #contact-main").addClass("scale-out");
+            
+            $("#about-blurb").show()
+            $("#about-blurb").addClass("slide-in-right")
+
+
+        } else if (clickID == "portfolio-main") {
+            $("#about-main, #contact-main").addClass("scale-out")
+
+        } else if (clickID == "contact-main") {
+            $("#about-main, #portfolio-main").addClass("scale-out")
+
+        }
+    })
 
 }); //END Document ready function
